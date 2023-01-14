0c3216e8e46562e0bc7f4fcb101975eb3e74ed38
C191239 <zsz2007314zsz@sina.com>
2023-01-14 15:08:18 +0800

    Initial commit.

---
+++ src/main/java/org/c191239/customitemmerge/CustomItemMerge.java
@@ -0,0 +1,10 @@
+package org.c191239.customitemmerge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomItemMerge implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/c191239/customitemmerge/mixin/ItemEntityMixin.java
@@ -0,0 +1,8 @@
+package org.c191239.customitemmerge.mixin;
+
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+}
